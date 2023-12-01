d96d8cbfe8f7621479bd7d1004d950d2d2c9b155
Robin Stocker <robin@nibor.org>
2023-12-02 00:10:19 +1100

    Initial import, some stuff from 2022

---
+++ src/test/java/aoc2023/Day01.java
@@ -0,0 +1,17 @@
+package aoc2023;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Day01 {
+
+    public static void main(String[] args) {
+
+    }
+
+    @Test
+    void example() {
+        assertEquals(1, 1);
+    }
+}
